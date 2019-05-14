2117796bc7b7578bb9a013b2e7ab55612869094a
didactor91 <didactor91@gmail.com>
2019-05-14 19:47:01 +0200

    upload to repo

---
+++ build/precache-manifest.80c1c416612a18172a9b897f29c88ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26295f03e7822f853730f57020a3e1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b01fc240f26577ce2778",
+    "url": "/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "28d06f239dfef93db00b",
+    "url": "/static/css/main.b084a555.chunk.css"
+  },
+  {
+    "revision": "b01fc240f26577ce2778",
+    "url": "/static/js/2.dd8a559b.chunk.js"
+  },
+  {
+    "revision": "28d06f239dfef93db00b",
+    "url": "/static/js/main.61e71e5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
