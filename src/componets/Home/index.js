e509b54b8ee32d11bd0b909a98d4ffbbe2a13a49
didactor91 <didactor91@gmail.com>
2019-05-16 09:44:15 +0200

    view

--- src/componets/Home/index.js
+++ src/componets/Home/index.js
@@ -3,28 +3,35 @@ import literals from './literals'
 import './index.sass'
 import emoji from 'react-easy-emoji'
 
-function Home({lang, onStopCode, onLineCode,onFavorites, onLogout}) {
-    const { title, stopByCode, stopByLine, favoriteStops, logout} = literals[lang]
+function Home({ lang, onStopCode, onLineCode, onFavorites, onLogout }) {
+    const { title, stopByCode, stopByLine, favoriteStops, logout } = literals[lang]
 
     return <section className="main-home">
-       <section className='buttons-landing buttons is-10-desktop is-10-tablet is-10-mobile is-offset-one-quarter'>
+        <section className='buttons-home buttons'>
 
             <h1 className="home-title">
                 <p>
-                    { emoji(' 📍 ... 🚍') }
+                    {emoji(' 📍 ... 🚍')}
                     <span>{title}</span>
                 </p>
-                
+
             </h1>
             <section className="button-section">
-                <button className='button-landing button is-primary is-large is-rounded'  onClick={()=>onStopCode()}>{stopByCode}</button>
+            <div className='columns is-8 is-2-offset is-desktop'>
+            <div className='column is-half is-desktop'>
+                <button className='button-landing button is-danger is-large is-rounded' onClick={() => onStopCode()}>{stopByCode}</button>
                 <hr></hr>
-                <button className='button-landing button is-primary is-large is-rounded'  onClick={()=>onLineCode()}>{stopByLine}</button>
+                <button className='button-landing button is-danger is-large is-rounded' onClick={() => onLineCode()}>{stopByLine}</button>
                 <hr></hr>
-                <button className='button-landing button is-primary is-large is-rounded'  onClick={()=>onFavorites()}>{favoriteStops}</button>
+                </div>
+                <div className='column is-half is-desktop'>
+                <button className='button-landing button is-danger is-large is-rounded' onClick={() => onFavorites()}>{favoriteStops}</button>
                 <hr></hr>
-                <button className='button-landing button is-primary is-large is-rounded'  onClick={()=>onLogout()}>{logout}</button>
-            </section>    
+                <button className='button-landing button is-danger is-large is-rounded' onClick={() => onLogout()}>{logout}</button>
+                <hr></hr>
+                </div>
+                </div>
+                </section>
         </section>
     </section>
 
